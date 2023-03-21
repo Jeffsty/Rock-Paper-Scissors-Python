5affbdcaa1cb0633d2c2c738f0de969dfbee01d6
Kirsty <kirsty.sees@gmail.com>
2023-03-21 14:11:12 +0000

    That's enough for today

--- main.py
+++ main.py
@@ -1,27 +1,36 @@
+import random
 # lets start with some pseudocode and TDD
 
 def main():
-    ...
+    compare_weapons()
+    play_round()
 
 
 def play_round():
-    ...
+    for i in range(5):
+        if i == 0:
+            i = i + 1
+            compare_weapons()
+        print(i)
 
 
 def play_game():
     ...
 
 
-def player_weapon_choice():
-    ...
-
-
-def program_weapon_choice():
-    ...
-
-
 def compare_weapons():
-    ...
+    weapon_list = ["Rock", "Paper", "Scissors"]
+    program_weapon = random.choice(weapon_list)
+    print(program_weapon)
+    player_weapon = input("Choose your weapon!\n").title()
+    if player_weapon == "Rock" or player_weapon == "Paper" or player_weapon == "Scissors":
+        print(f"You chose: {player_weapon}")
+    else:
+        print("Not a valid weapon")
+    if player_weapon == program_weapon:
+        print("It's a draw!")
+    else:
+        print("Let's try and get a draw next time")
 
 
 if __name__ == "__main__":
--- test_main.py
+++ test_main.py
@@ -4,3 +4,6 @@ import pytest
 def main():
     ...
     # call the other functions here
+
+def test_compare_weapons():
+    assert main.compare_weapons("It's a draw") == True
